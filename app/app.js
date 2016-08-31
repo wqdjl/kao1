bef5b1634041a587408a1847525e87a30c0d21ce
miao.deng <miaomiao618100@gmail.com>
2016-08-31 14:23:54 +0800

    add folder

--- app/app.js
+++ app/app.js
@@ -1,9 +1,26 @@
 import koa from  'koa';
+import logger from 'koa-logger';
+import onerror from 'koa-onerror';
+import staticFileServer from 'koa-static';
+import bodyParser from 'koa-bodyParser'; 
+
 
 let app=koa();
 
-app.use(function *(){
-    this.body='123'
-});
+// app.use(function *(next){
+//     yield next;
+//    //throw new Error(12313);
+//    //this.body='123'
+// });
+app.use(bodyParser());
+
+app.use(staticFileServer(__dirname+'/public'));
+
+app.use(logger());
+
+// app.on('error',(err,ctx)=>{
+//     console.log("error:"+err+'\r\nend');
+// });
+onerror(app);
 
 app.listen(3000);
